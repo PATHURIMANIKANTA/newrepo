abfb928b375c4914fe56d4be3d446cf8525bdba3
manikanta <pathurimanikanta>
2022-02-24 22:04:16 +0530

    repo

---
+++ Emedicalstore/src/main/java/com/medic/db/MedicineRepository.java
@@ -0,0 +1,9 @@
+package com.medic.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.medic.model.Medicine;
+
+
+public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+}
